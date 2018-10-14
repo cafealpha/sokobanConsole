3e646889303046b2ba3079640d34715fd5f5e621
cafealpha <cyanine@naver.com>
2018-10-15 07:33:41 +0900

    기본 이동 구현

--- sokobanConsole/sokobanConsole.h
+++ sokobanConsole/sokobanConsole.h
@@ -23,6 +23,12 @@ struct stageData
 	int mapHeight;
 };
 
+struct pos
+{
+	int x;
+	int y;
+};
+
 //링크드리스트 구현
 struct node
 {
@@ -35,6 +41,11 @@ node* createNode(mobj *item);
 void deleteList(node *head);
 void addNode(node *head, node *item);
 void printList(node *head);
+//좌표위치에 원하는 오브젝트가 있는지 찾아주는 함수
+mobj* findObjectAtPos(node* head, pos Pos);
+
+node* boxList;
+
 
 
 //맵데이터에서 맵을 추출한 맵
@@ -55,10 +66,13 @@ void drawMap();
 void drawPlayer();
 void release();
 
+void updateGameMap();
+
 //방향을 입력받아서 움직임
 void move(mobj *obj, int dx, int dy);
 //움직일 수 있는지 체크
 bool canMove(mobj *obj, int dx, int dy);
 
-
-
+//좌표를 입력받아 배열인덱스를 반환하는 함수
+int calcPosToIndex(int x, int y);
+pos indexToPos(int index);
