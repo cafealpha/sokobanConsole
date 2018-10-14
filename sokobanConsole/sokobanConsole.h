9a55c52c30341f9a0f34287dec1089b2a3ba9e03
cafealpha <cyanine@naver.com>
2018-10-14 19:24:16 +0900

    메모리 동적할당 에러 수정

--- sokobanConsole/sokobanConsole.h
+++ sokobanConsole/sokobanConsole.h
@@ -1,8 +1,59 @@
 #pragma once
 
+using namespace std;
+
+enum mapObj {
+	E_WALL,
+	E_SPACE,
+	E_BOX,
+	E_GOAL,
+	E_PLAYER,
+};
+struct mobj
+{
+	mapObj type;
+	int x, y;
+	bool status;
+};
+
+struct stageData
+{
+	char *mapData;
+	int mapWidth;
+	int mapHeight;
+};
+
+//링크드리스트 구현
+struct node
+{
+	mobj *item;
+	struct node *next;
+};
+
+node* createList();
+node* createNode(mobj *item);
+void deleteList(node *head);
+void addNode(node *head, node *item);
+void printList(node *head);
+
+
+//맵데이터에서 맵을 추출한 맵
+int *map;
+//화면에 표시되는 맵
+int *dMap;
+//플레이어 데이터
+mobj *player;
+
+
+stageData stage1;
+
+
+
 int main();
 
 void init();
 void update();
 void render();
+void drawMap();
+void drawPlayer();
 
